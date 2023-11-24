edb6ed23d2a74f0c1f0aa7b140efd22621ec45d4
RosieOh <20172207@gm.hannam.ac.kr>
2023-11-24 09:29:33 +0900

    🌈setting : 프론트엔드 초기 메뉴 설정🌈

---
+++ src/components/layout/header.jsx
@@ -0,0 +1,14 @@
+export default function Head() {
+    return (
+        <>
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0"
+            ></meta>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <title>Tsherpa - IntraNet</title>
+            <meta name="description" content="티 셀파 교수학습지원센터" />
+        </>
+    );
+}
